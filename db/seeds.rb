e5ad2b7d893b6823c3206865c155f6eeac77db1b
ullahasad386 <ullahasad386@gmail.com>
2019-06-03 03:37:36 +0500

    Build users resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+User.create(username: "Asad", password: "password")
+User.create(username: "Asim", password: "password")
+User.create(username: "Hamza", password: "password")
+User.create(username: "Muaaz", password: "password")
+User.create(username: "Faizan", password: "password")
