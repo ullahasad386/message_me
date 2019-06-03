1f089b99602fc7e2f45cdce1c4074127b3ae70c2
ullahasad386 <ullahasad386@gmail.com>
2019-06-03 10:56:54 +0500

    Add messages model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :username, presence: true, length: { minimum: 3, maximum: 20 },
+            uniqueness: { case_sensitive: false }
+  has_many :messsages
   has_secure_password
 end
