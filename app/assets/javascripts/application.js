ca51fedfd3e381a9fe2d4ad5c32dd5520cfad536
ullahasad386 <ullahasad386@gmail.com>
2019-06-03 13:09:32 +0500

    Session login, logout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,4 +20,11 @@
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+
+  $('.message .close').on('click', function() {
+      $(this).closest('.message').transition('fade')
+      ;
+    })
+  ;
+
 })
